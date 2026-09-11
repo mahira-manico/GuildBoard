bcc7e1480e4eda6960be0ca6c9ce35b28c43cb74
mahira-manico <mahira.manico@laplateforme.io>
2026-09-11 15:30:10 +0200

    chores: delete unused imports

--- src/main/java/com/laplateforme/guildboard/application/controller/AssignmentController.java
+++ src/main/java/com/laplateforme/guildboard/application/controller/AssignmentController.java
@@ -1,4 +1,6 @@
 package com.laplateforme.guildboard.application.controller;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class AssignmentController {
 }
