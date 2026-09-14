0c664707b3ae2567c2ad9906ea49e38b0b29cf75
moinahalima-abdou <moina-halima.abdou-doukeini@laplateforme.io>
2026-09-14 09:23:43 +0200

    Update Adventurer request DTO

--- src/main/java/com/laplateforme/guildboard/application/dto/AdventurerRequestDTO.java
+++ src/main/java/com/laplateforme/guildboard/application/dto/AdventurerRequestDTO.java
@@ -1,4 +1,25 @@
 package com.laplateforme.guildboard.application.dto;
+import com.laplateforme.guildboard.application.entity.CharacterClass;
 
 public class AdventurerRequestDTO {
+
+    private String name;
+    private CharacterClass characterClass;
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+     // Setters
+    public CharacterClass getCharacterClass() {
+        return characterClass;
+    }
+
+    public void setCharacterClass(CharacterClass characterClass) {
+        this.characterClass = characterClass;
+    }
 }
