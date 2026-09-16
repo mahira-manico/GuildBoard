1f23737f82da40b36181e91f23b560ab385f9de4
mahira-manico <mahira.manico@laplateforme.io>
2026-09-16 14:26:06 +0200

    chores: rename of application className by name of the project

---
+++ src/main/java/com/laplateforme/guildboard/application/GuildBoardApplication.java
@@ -0,0 +1,13 @@
+package com.laplateforme.guildboard.application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class GuildBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuildBoardApplication.class, args);
+	}
+
+}
